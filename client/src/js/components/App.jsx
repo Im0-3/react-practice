d3faca8aaf2a762841edc10316f8735c4ccf2639
UNIEL Yusuke Omi <yusuke_omi@uniel.jp>
2016-10-21 11:40:43 +0900

    [chore]: add constructor to App Component and Slider Component

--- client/src/js/components/App.jsx
+++ client/src/js/components/App.jsx
@@ -2,9 +2,33 @@ import React from 'react';
 import Slider from './Slider.jsx';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   getInitialState (){
     return {
-      name: hoge
+      slide: [
+        {
+          title: 'A',
+          image: 'hoge.png'
+        },
+        {
+          title: 'B',
+          image: 'hoge.png'
+        },
+        {
+          title: 'C',
+          image: 'hoge.png'
+        },
+        {
+          title: 'D',
+          image: 'hoge.png'
+        },
+        {
+          title: 'E',
+          image: 'hoge.png'
+        }
+      ]
     }
   }
   render() {
--- client/src/js/components/Slider.jsx
+++ client/src/js/components/Slider.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return <div>{this.props.name}</div>;
   }
